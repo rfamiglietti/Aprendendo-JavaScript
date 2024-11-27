a907500500e0e4ef87e0471466ad405ee239329c
rfamiglietti <romulopfami@gmail.>
2024-11-26 21:19:19 -0300

    ajuste do desafio relampago

--- JavaAnderson/DOM/desafiorelampago.js
+++ JavaAnderson/DOM/desafiorelampago.js
@@ -5,23 +5,10 @@ const botao = document.getElementById("botao");
 
 // Adiciona um evento de clique no botão
 botao.addEventListener("mouseover", () => {
-    // Verifica se a caixa tem a classe "visivel"
-    if (caixa.classList.contains("visivel")) {
-        // Troca a classe para "oculto"
-        caixa.classList.replace("visivel", "oculto");
-    } else {
-        // Troca a classe para "visivel"
-        caixa.classList.replace("oculto", "visivel");
-    }
+    caixa.classList.replace("oculto", "visivel");
 });
 
 botao.addEventListener("mouseout", () => {
-    // Verifica se a caixa tem a classe "visivel"
-    if (caixa.classList.contains("visivel")) {
-        // Troca a classe para "oculto"
-        caixa.classList.replace("visivel", "oculto");
-    } else {
-        // Troca a classe para "visivel"
-        caixa.classList.replace("oculto", "visivel");
-    }
+    caixa.classList.replace("visivel", "oculto");
+    
 });
